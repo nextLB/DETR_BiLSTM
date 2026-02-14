35200111cda21b5f46b78899815a982853f74155
next <364574103@qq.com>
2026-02-14 10:28:44 +0800

    0214

--- train/tools/BaseConfig.py
+++ train/tools/BaseConfig.py
@@ -3,6 +3,10 @@
 
 import os
 import yaml
+import copy
+from typing import Any, Dict, Optional, List
+
+INCLUDE_KEY = '__include__'
 
 
 
@@ -15,16 +19,45 @@ def load_config(filePath, cfg=dict()):
         if fileCfg is None:
             return {}
 
-    print(fileCfg)
+    if INCLUDE_KEY in fileCfg:
+        baseYamls = list(fileCfg[INCLUDE_KEY])
+        for baseYaml in baseYamls:
+            if baseYaml.startswith('~'):
+                baseYaml = os.path.expanduser(baseYaml)
+
+            if not baseYaml.startswith('/'):
+                baseYaml = os.path.join(os.path.dirname(filePath), baseYaml)
+
+            with open(baseYaml) as f:
+                baseCfg = yaml.load(f, Loader=yaml.Loader)
+                merge_dict(cfg, baseCfg)
+
+    return merge_dict(cfg, fileCfg)
+
+
+def merge_dict(dct, another_dct, inplace=True) -> Dict:
+    def _merge(dct, another) -> Dict:
+        for k in another:
+            if (k in dct and isinstance(dct[k], dict) and isinstance(another[k], dict)):
+                _merge(dct[k], another[k])
+            else:
+                dct[k] = another[k]
+
+        return dct
+
+    if not inplace:
+        dct = copy.deepcopy(dct)
+    return _merge(dct, another_dct)
+
 
 
 
 
 def main():
-    TEST_CONFIG_FILE_PATH = '/home/next/桌面/RT-DETR/RT-DETR/rtdetrv2_pytorch/configs/rtdetrv2/rtdetrv2_r18vd_120e_voc.yml'
+    TEST_CONFIG_FILE_PATH = '/home/next/桌面/RT-DETR/DETR_BiLSTM/train/config_files/rtdetrv2_r18vd_120e_voc.yml'
 
-    load_config(TEST_CONFIG_FILE_PATH)
-
+    cfg = load_config(TEST_CONFIG_FILE_PATH)
+    print(cfg['task'])
 
 
 
