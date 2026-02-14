d6adbcd85102b34340a5380b42813ab4bd15da77
next <364574103@qq.com>
2026-02-14 09:15:00 +0800

    temp load config

--- train/tools/BaseConfig.py
+++ train/tools/BaseConfig.py
@@ -1,11 +1,30 @@
 
 
 
+import os
+import yaml
+
+
+
+def load_config(filePath, cfg=dict()):
+    _, ext = os.path.splitext(filePath)
+    assert ext in ['.yml', '.yaml'], "only support yaml files"
+
+    with open(filePath) as f:
+        fileCfg = yaml.load(f, Loader=yaml.Loader)
+        if fileCfg is None:
+            return {}
+
+    print(fileCfg)
+
 
 
 
 def main():
-    pass
+    TEST_CONFIG_FILE_PATH = '/home/next/桌面/RT-DETR/RT-DETR/rtdetrv2_pytorch/configs/rtdetrv2/rtdetrv2_r18vd_120e_voc.yml'
+
+    load_config(TEST_CONFIG_FILE_PATH)
+
 
 
 
